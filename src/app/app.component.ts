9bc660f33df97e4feb062128b81775fe1e666a07
AnnaFerreira18 <anninhalacerda5@gmail.com>
2024-01-17 18:54:55 -0300

    config HTTP client/providers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MediaComponent } from "./media/media.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, MatToolbarModule, MediaComponent]
 })
 export class AppComponent {
   title = 'renda-app';
