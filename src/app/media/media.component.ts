95def03cc2a528273085aa7188e0b87936c904f2
AnnaFerreira18 <anninhalacerda5@gmail.com>
2024-01-17 18:54:40 -0300

    metodos dos componentes media/user

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { MediaService } from './media.service';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-media',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
+  providers:[],
   templateUrl: './media.component.html',
   styleUrl: './media.component.scss'
 })
 export class MediaComponent {
 
+  listPeople: boolean = false;
+  listMedia: boolean = false;
+  del: boolean = false;
+  listaP : any[] = [];
+  displayedColumns: string[] = ['position', 'nome', 'raca', 'renda'];
+
+  constructor(private mediaService : MediaService ){}
+
+  listar(){
+    console.log("Lista criada")
+    this.mediaService.obterTodosUsuarios().subscribe((p)=>{
+      console.log(p)
+      if(p){
+        this.listaP = p.sort((a: { id: number; } , b: { id: number; }) => a.id - b.id);
+        this.listPeople = true;
+
+      }
+    })
+  }
+  Media(){
+    console.log("Media criada")
+    this.mediaService.calcularMediaRenda().subscribe((p)=>{
+      console.log(p)
+    })
+  }
+  Deletar(){
+    console.log("Delete criado")
+  }
 }
